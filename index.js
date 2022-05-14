0b7093ba9536bbb0cfc142b1248c7ca66f6d586c
Heather <hmcleod29@gmail.com>
2022-05-14 15:55:47 -0400

    Final changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const breedName = process.argv[2];
 
 fetchBreedDescription(breedName, (error, desc) => {
   if (error) {
-    console.log(`Error found! ${error}`);
+    console.log(`${error}`);
   } else {
     console.log(desc);
   }
