983e9e4cc20c8592a1d06392063387fb612587f2
Heather <hmcleod29@gmail.com>
2022-05-14 13:19:43 -0400

    Assignment completed, code runs. Next step is seeing if a mentor can help me clean it up

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,21 @@
 const request = require('request');
 const catName = process.argv.slice(2);
-console.log(catName);
+// console.log(catName);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${catName}`, (error, response, body) => {
+request(`https://api.theatapi.com/v1/breeds/search?q=${catName}`, (error, response, body) => {
   // console.log('body:', body); 
   // console.log(typeof body)
   if (body) {
     const data = JSON.parse(body);
     // console.log(data);
     if (data.length < 1) {
-      console.log('Error! Cat not found.');
+      console.log('Oops! Cat not found.');
     } else {
       console.log(data[0].description);
     }
+  } else {
+    console.log(`Error found: ${error}`);
+
   }
 
 });
