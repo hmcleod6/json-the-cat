3a631961df96735d65deb223a0394e89e1c614eb
Heather <hmcleod29@gmail.com>
2022-05-14 15:24:49 -0400

    Refactoring code and removing console.logs. Adding all the user-facing logic to index.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,11 +1,21 @@
 const request = require('request');
-const catName = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${catName}`, (error, response, body) => {
-  if (body) {
+
+const fetchBreedDescription = (breedName, callback) => {
+
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    if (response.statusCode !== 200) {
+      return callback(response.statusCode, null);
+    }
     const data = JSON.parse(body);
-    data.length ? console.log(data[0].description) : console.log('Oops! Cat not found!');
-  } else {
-    console.log(`Error found: ${error}`);
-  }
-});
+    if (!data[0]) {
+      return callback(`No such breed: ${breedName} is not found`, null);
+    }
+    return callback(null, data[0].description);
+
+  });
+}
+  module.exports = { fetchBreedDescription };
