6e8f1405e9b172a2fd06812a39a0339fc54009d9
Heather <hmcleod29@gmail.com>
2022-05-14 13:38:18 -0400

    Cleaned up code with ternary operator with help of mentor! :)

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,11 @@
 const request = require('request');
 const catName = process.argv.slice(2);
-// console.log(catName);
 
-request(`https://api.theatapi.com/v1/breeds/search?q=${catName}`, (error, response, body) => {
-  // console.log('body:', body); 
-  // console.log(typeof body)
+request(`https://api.thecatapi.com/v1/breeds/search?q=${catName}`, (error, response, body) => {
   if (body) {
     const data = JSON.parse(body);
-    // console.log(data);
-    if (data.length < 1) {
-      console.log('Oops! Cat not found.');
-    } else {
-      console.log(data[0].description);
-    }
+    data.length ? console.log(data[0].description) : console.log('Oops! Cat not found!');
   } else {
     console.log(`Error found: ${error}`);
-
   }
-
 });
